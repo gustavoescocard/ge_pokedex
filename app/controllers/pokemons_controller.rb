371cb0db362ea63318615686785c3436b27db9d9
Gustavo Escocard <gustavo.escocard@gmail.com>
2023-03-22 00:15:35 -0300

    Add some style and poke images

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,8 +1,12 @@
 class PokemonsController < ApplicationController
   def index
-    all_poke_entries = PokeApi.get(pokedex: 1).pokemon_entries
-    poke_entries_qtt = all_poke_entries.count
+    national_dex = PokeApi.get(pokedex: 1).pokemon_entries.collect(&:pokemon_species)
 
-    @pokemons = all_poke_entries.collect(&:pokemon_species)
+    @poke_names = []
+    @poke_images = []
+    (0..150).each do |poke_number|
+      @poke_names << national_dex[poke_number].name.capitalize
+      @poke_images << PokeApi.get(pokemon: poke_number + 1).sprites.front_default
+    end
   end
 end
